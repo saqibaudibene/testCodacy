9cf916ba70949623069d61c9a93691a6274a3fb0
Saqib Usman <saqib.usman@audibene.de>
2019-02-19 10:22:33 +0100

    test codacy project

---
+++ TestCodacy/TestCodacy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCodacy
+//
+//  Created by Saqib Usman on 19/02/2019.
+//  Copyright © 2019 Saqib Usman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
